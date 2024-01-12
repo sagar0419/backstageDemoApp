023a6c0e649d24b04c9b04384051f126fdb498cc
Sagar Parmar <Sagar Parmar>
2024-01-12 21:55:05 +0530

    ec2 template

--- packages/backend/src/plugins/scaffolder.ts
+++ packages/backend/src/plugins/scaffolder.ts
@@ -3,12 +3,29 @@ import { createRouter } from '@backstage/plugin-scaffolder-backend';
 import { Router } from 'express';
 import type { PluginEnvironment } from '../types';
 
+// import { createAwsS3CpAction, createEcrAction, createAwsSecretsManagerCreateAction } from '@roadiehq/scaffolder-backend-module-aws';
+
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
   const catalogClient = new CatalogClient({
     discoveryApi: env.discovery,
   });
+
+  // const actions = [
+  //   createAwsS3CpAction(),
+  //   createEcrAction(),
+  //   createAwsSecretsManagerCreateAction(),
+  //   ...createBuiltinActions({
+  //     containerRunner,
+  //     integrations,
+  //     config,
+  //     catalogClient,
+  //     reader,
+  //   }),
+  // ];
+
+  
 
   return await createRouter({
     logger: env.logger,
@@ -18,5 +35,12 @@ export default async function createPlugin(
     catalogClient,
     identity: env.identity,
     permissions: env.permissions,
+    // containerRunner,
+    // logger,
+    // config,
+    // database,
+    // catalogClient,
+    // reader,
+    // actions,
   });
 }
